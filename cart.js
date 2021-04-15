d33a27aa60bfcde5212a85378dd2d2a17e02a56a
Belyaev Mikhail <68550956+BelyaevMikhail@users.noreply.github.com>
2021-04-15 11:41:11 +0300

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,3 +1,14 @@
+const cartItem = {
+    render(good) {
+        return `<div class="good">
+                    <div><b>Наименование</b>: ${good.product_name}</div>
+                    <div><b>Цена за шт.</b>: ${good.price}</div>
+                    <div><b>Количество</b>: ${good.quantity}</div>
+                    <div><b>Стоимость</b>: ${good.quantity * good.price}</div>
+                </div>`;
+    }
+}
+
 const cart = {
     cartListBlock: null,
     clearCartButton: null,
@@ -7,46 +18,46 @@ const cart = {
             id_product: 123,
             product_name: "доска",
             price: 300,
-            quantity: 1
+            quantity: 1,
         },
         {
             id_product: 789,
             product_name: "бревно",
             price: 500,
-            quantity: 1
+            quantity: 1,
         },
         {
             id_product: 456,
             product_name: "брус",
             price: 700,
-            quantity: 1
-        }
+            quantity: 1,
+        },
     ],
-    init() { 
-        this.cartListBlock = document.querySelector(selectors '.cart-list');
-        this.clearCartButton = document/querySelector(selectors '.cart-btn');
-        this.clearCartButton.addEventListenner('click', () =>  this.clearCart());
+    init() {
+        this.cartListBlock = document.querySelector('.cart-list');
+        this.clearCartButton = document.querySelector('.cart-btn');
+        this.clearCartButton.addEventListener('click', () => this.clearCart());
 
         this.render();
     },
     render() {
         if (this.goods.length) {
             this.goods.forEach(good => {
-                this.cartListBlock.insertAjacentHTML(where: 'beforeend', this.cartItem.render(good));
+                this.cartListBlock.insertAdjacentHTML('beforeend', this.cartItem.render(good));
             });
-            this.cartListBlock.insertAjacentHTML(where: html: 'В корзине ${this.goods.lenght} позиций(a) стоимостью ${this.getCartPrise()}}');
+            this.cartListBlock.insertAdjacentHTML('beforeend', `В корзине ${this.goods.length} позиций(а) стоимостью ${this.getCartPrice()}`);
         } else {
-            this.cartListBlock.textContent = 'Корзина пуста'
+            this.cartListBlock.textContent = 'Корзина пуста';
         }
-     },
-    getCartPrice() { 
-        return this.goods.reduce(function (price: number , good: {...}) {
-            return price = good.price *good.quantity;
+    },
+    getCartPrice() {
+        return this.goods.reduce(function (price, good) {
+            return price + good.price * good.quantity;
         }, 0);
     },
     clearCart() {
-        this.goods =[];
+        this.goods = [];
         this.render();
-     },
-
-},
+    },
+};
+cart.init();
